555d1d48176347d8eaa1cc01d54ceffee8a55a03
Mae830 <55216013+Mae830@users.noreply.github.com>
2021-02-18 23:23:28 -0500

    Added a locations controller/view: index/show/new

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,5 +5,22 @@ class SearchController < ApplicationController
   def show
     @location = Location.find(params[:id])
   end
+  def new
+    @location = Location.new
+  end
+  def create
+    @location = Location.new(location_params)
+
+    if @location.save
+      redirect_to @location
+    else
+      render :new
+    end
+  end
+
+  private
+    def location_params
+      params.require(:location).permit(:name, :street1, :street2, :city, :state, :zip)
+    end
 end
 
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,7 @@
 class Location < ApplicationRecord
+    validates :name, presence: true
+    validates :street1, presence: true
+    validates :city, presence: true
+    validates :state, presence: true
+    validates :zip, presence: true
 end

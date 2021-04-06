9eb057dd0c94cda43bff2b00257e869406f10fda
Mae830 <55216013+Mae830@users.noreply.github.com>
2021-04-05 21:53:08 -0400

    Fixed bootstrap, started styling  login/background

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -7,6 +7,10 @@ const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
+    jQuery: 'jquery/src/jquery',
+    Popper: ['popper.js', 'default']
   })
 )
+
+
+module.exports = environment
